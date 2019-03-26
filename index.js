2efa236bf7bb771e1775ad85ffe1ba2f5f3b0677
millionape <kittaporn.j@ku.th>
2019-03-26 12:22:32 +0700

    fixing date time

--- index.js
+++ index.js
@@ -82,7 +82,8 @@ server.on('message', (msg, rinfo) => {
     console.log(arr.length);
     if(arr.length >= 11){
       var thTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Bangkok"});
-      var jsonDat = {gid:arr[10].toString(),id:arr[0].toString(),airTemp:arr[4].toString(),airHumid:arr[5].toString(),pm1:arr[1].toString(),pm25:arr[2].toString(),pm10:arr[3].toString(),rain:arr[7].toString(),uv:arr[6].toString(),soilHumid:arr[8].toString(),wind:arr[9].toString(),dt:thTime};
+      var now = new Date(thTime);
+      var jsonDat = {gid:arr[10].toString(),id:arr[0].toString(),airTemp:arr[4].toString(),airHumid:arr[5].toString(),pm1:arr[1].toString(),pm25:arr[2].toString(),pm10:arr[3].toString(),rain:arr[7].toString(),uv:arr[6].toString(),soilHumid:arr[8].toString(),wind:arr[9].toString(),dt:now};
       console.log(jsonDat);
       // try {
       //   JSON.parse(jsonDat);
