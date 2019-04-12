4cbc948bff36dbcbcef51ab18741acff255fd48e
millionape <kittaporn.jp@gmail.com>
2019-04-12 23:15:15 +0700

    fix time 1

--- index.js
+++ index.js
@@ -86,8 +86,8 @@ server.on('message', (msg, rinfo) => {
       //var time = moment.tz('Asia/Bangkok').format("YYYY-MM-DDTHH:MM:ss");
       //var now = moment(new Date());
       //var th = now.tz('Asia/Bangkok').format();
-      var thTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Bangkok"});
-      var now = new Date(thTime);
+      //var thTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Bangkok"});
+      var now = new Date(Date.now());
       var jsonDat = {gid:arr[10].toString(),id:arr[0].toString(),airTemp:arr[4].toString(),airHumid:arr[5].toString(),pm1:arr[1].toString(),pm25:arr[2].toString(),pm10:arr[3].toString(),rain:arr[7].toString(),uv:arr[6].toString(),soilHumid:arr[8].toString(),wind:arr[9].toString(),dt:now};
       console.log(jsonDat);
       // try {
