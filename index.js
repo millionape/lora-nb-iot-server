31149e0d292e36cf73eb90666312b373e90aff37
millionape <kittaporn.jp@gmail.com>
2019-05-13 00:51:14 +0700

    date fix

--- index.js
+++ index.js
@@ -86,9 +86,9 @@ server.on('message', (msg, rinfo) => {
       //var time = moment.tz('Asia/Bangkok').format("YYYY-MM-DDTHH:MM:ss");
       //var now = moment(new Date());
       //var th = now.tz('Asia/Bangkok').format();
-      var thTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Bangkok"});
-      var now = new Date(thTime);
-      var jsonDat = {gid:arr[10].toString(),id:arr[0].toString(),airTemp:arr[4].toString(),airHumid:arr[5].toString(),pm1:arr[1].toString(),pm25:arr[2].toString(),pm10:arr[3].toString(),rain:arr[7].toString(),uv:arr[6].toString(),soilHumid:arr[8].toString(),wind:arr[9].toString(),dt:now.toLocaleString()};
+      //var thTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Bangkok"});
+      //var now = new Date(thTime);
+      var jsonDat = {gid:arr[10].toString(),id:arr[0].toString(),airTemp:arr[4].toString(),airHumid:arr[5].toString(),pm1:arr[1].toString(),pm25:arr[2].toString(),pm10:arr[3].toString(),rain:arr[7].toString(),uv:arr[6].toString(),soilHumid:arr[8].toString(),wind:arr[9].toString(),dt:new Date()};
       console.log(jsonDat);
       // try {
       //   JSON.parse(jsonDat);
